8696b79a0141010f0f480167eb742e78996eb3ec
NicolasGabrielDev <nicolascefet@hotmail.com>
2021-01-28 18:14:30 -0300

    28012021

--- src/screens/SessionQuestions.js
+++ src/screens/SessionQuestions.js
@@ -8,6 +8,7 @@ import { ScrollView } from 'react-native-gesture-handler'
 
 export default function SessionQuestions({ navigation }) {
     const [visible, setVisible] = React.useState(false)
+    const [quantidade, setQuantidade] = React.useState(2)
     const [checked, setChecked] = React.useState('numerica')
     const [isLoading, setIsLoading] = React.useState(true)
 
@@ -15,38 +16,60 @@ export default function SessionQuestions({ navigation }) {
     const [usuario, setUsuario] = React.useState('')
     const [res, setRes] = React.useState(null)
 
+    async function fetchData() {
+        const codigo = await AsyncStorage.getItem('codigo')
+        const token = await AsyncStorage.getItem('userToken')
+
+        setCodigo(codigo)
+
+        await api.post('/api/perguntas', {
+            codigo
+        }, {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': "Bearer " + token,
+            }
+        }).then(response => {
+            const { res, usuario } = response.data
+            setUsuario(usuario)
+            setRes(res)
+        }).catch(error => {
+            console.log(error.response)
+        })
+        setCodigo(codigo)
+        setIsLoading(false)
+
+        return async () => {
+            AsyncStorage.removeItem('codigo')
+        };
+    }
+
+    async function criarPergunta() {
+        const token = await AsyncStorage.getItem('userToken')
+
+        await api.post('/api/criar-pergunta', {
+            "tipo": checked,
+            "quantidade": quantidade,
+            "sessao_id": res.id
+        }, {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': "Bearer " + token
+            }
+        }).then(response => {
+            const { res } = response.data
+            alert(res)
+            fetchData()
+            setVisible(false)
+        }).catch(error => {
+            console.log(error)
+        })
+    }
+
     useFocusEffect(
         React.useCallback(() => {
-            async function fetchData() {
-                const codigo = await AsyncStorage.getItem('codigo')
-                setCodigo(codigo)
-
-                const token = await AsyncStorage.getItem('userToken')
-
-                await api.post('/api/perguntas', {
-                    codigo
-                }, {
-                    headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json',
-                        'Authorization': "Bearer " + token,
-                    }
-                }).then(response => {
-                    const { res, usuario } = response.data
-                    console.log(usuario)
-                    console.log(res)
-                    setUsuario(usuario)
-                    setRes(res)
-                }).catch(error => {
-                    console.log(error.response)
-                })
-                setCodigo(codigo)
-                setIsLoading(false)
-
-                return async () => {
-                    AsyncStorage.removeItem('codigo')
-                };
-            }
             fetchData()
         }, [])
     );
@@ -55,10 +78,12 @@ export default function SessionQuestions({ navigation }) {
         return (
             <View style={{
                 flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
                 width: '100%',
                 backgroundColor: '#ffffff'
             }}>
-                <ActivityIndicator size={64} color='blue'>
+                <ActivityIndicator size={64} color='#27a0ff'>
 
                 </ActivityIndicator>
             </View>
@@ -69,82 +94,90 @@ export default function SessionQuestions({ navigation }) {
         if (usuario == 'admin') {
             return (
                 <View style={styles.container}>
-                    <Modal style={styles.modalView} visible={visible} onDismiss={() => setVisible(false)}>
-                        <View style={styles.modalContainer}>
-                            <Text style={styles.title}>JustChoice</Text>
-                            <Text style={styles.subTitle}>Rápido e fácil de responder...</Text>
-                            <Text style={styles.subTitle}>Faça sua pergunta por aqui!</Text>
-                            <Text style={styles.textQuestion}>Qual a opção?</Text>
-                            <View style={[styles.radioContainer, { paddingLeft: 40 }]}>
-                                <RadioButton
-                                    value='numerica'
-                                    onPress={() => setChecked('numerica')}
-                                    status={checked === 'numerica' ? 'checked' : 'unchecked'}>
-                                </RadioButton>
-                                <Text>Númerica</Text>
+                    <ScrollView>
+
+                        <Modal style={styles.modalView} visible={visible} onDismiss={() => setVisible(false)}>
+                            <View style={styles.modalContainer}>
+                                <Text style={styles.title}>JustChoice</Text>
+                                <Text style={styles.subTitle}>Rápido e fácil de responder...</Text>
+                                <Text style={styles.subTitle}>Faça sua pergunta por aqui!</Text>
+                                <Text style={styles.textQuestion}>Qual a opção?</Text>
+                                <View style={[styles.radioContainer, { paddingLeft: 40 }]}>
+                                    <RadioButton
+                                        value='numerica'
+                                        onPress={() => setChecked('numerica')}
+                                        status={checked === 'numerica' ? 'checked' : 'unchecked'}>
+                                    </RadioButton>
+                                    <Text>Númerica</Text>
+                                </View>
+                                <View style={[styles.radioContainer, { paddingLeft: 40 }]}>
+                                    <RadioButton
+                                        value='alfabetica'
+                                        onPress={() => setChecked('alfabetica')}
+                                        status={checked === 'alfabetica' ? 'checked' : 'unchecked'}>
+                                    </RadioButton>
+                                    <Text>Alfabética</Text>
+                                </View>
+                                <View style={[styles.radioContainer, { paddingLeft: 40 }]}>
+                                    <RadioButton
+                                        value='dificuldade'
+                                        onPress={() => setChecked('dificuldade')}
+                                        status={checked === 'dificuldade' ? 'checked' : 'unchecked'}>
+                                    </RadioButton>
+                                    <Text>Níveis de Dificuldade</Text>
+                                </View>
+                                <View style={[styles.radioContainer, { paddingLeft: 40 }]}>
+                                    <RadioButton
+                                        value='qualidade'
+                                        onPress={() => setChecked('qualidade')}
+                                        status={checked === 'qualidade' ? 'checked' : 'unchecked'}>
+                                    </RadioButton>
+                                    <Text>Níveis de Qualidade</Text>
+                                </View>
+                                <View style={[styles.radioContainer, { paddingLeft: 40 }]}>
+                                    <RadioButton
+                                        value='"simnao'
+                                        onPress={() => setChecked('simnao')}
+                                        status={checked === 'simnao' ? 'checked' : 'unchecked'}>
+                                    </RadioButton>
+                                    <Text>Sim ou Não</Text>
+                                </View>
+                                <Text style={styles.textQuestion}>E a quantidade?</Text>
+                                <TextInput
+                                    style={styles.input}
+                                    placeholder="2"
+                                    value={quantidade}
+                                    keyboardType='number-pad'
+                                    onChangeText={quantidade => setQuantidade(quantidade)}
+                                ></TextInput>
+                                <View style={{ flexDirection: "row", alignItems: "center", alignSelf: "center" }}>
+                                    <TouchableOpacity style={[styles.button2, { backgroundColor: 'red', marginRight: 20, }]} onPress={() => setVisible(false)}>
+                                        <Text style={styles.textButton2}>CANCELAR</Text>
+                                    </TouchableOpacity>
+                                    <TouchableOpacity
+                                        style={styles.button2}
+                                        onPress={() => {
+                                            criarPergunta()
+                                        }}>
+                                        <Text style={styles.textButton2}>CRIAR</Text>
+                                    </TouchableOpacity>
+                                </View>
                             </View>
-                            <View style={[styles.radioContainer, { paddingLeft: 40 }]}>
-                                <RadioButton
-                                    value='alfabetica'
-                                    onPress={() => setChecked('alfabetica')}
-                                    status={checked === 'alfabetica' ? 'checked' : 'unchecked'}>
-                                </RadioButton>
-                                <Text>Alfabética</Text>
-                            </View>
-                            <View style={[styles.radioContainer, { paddingLeft: 40 }]}>
-                                <RadioButton
-                                    value='dificuldade'
-                                    onPress={() => setChecked('dificuldade')}
-                                    status={checked === 'dificuldade' ? 'checked' : 'unchecked'}>
-                                </RadioButton>
-                                <Text>Níveis de Dificuldade</Text>
-                            </View>
-                            <View style={[styles.radioContainer, { paddingLeft: 40 }]}>
-                                <RadioButton
-                                    value='qualidade'
-                                    onPress={() => setChecked('qualidade')}
-                                    status={checked === 'qualidade' ? 'checked' : 'unchecked'}>
-                                </RadioButton>
-                                <Text>Níveis de Qualidade</Text>
-                            </View>
-                            <View style={[styles.radioContainer, { paddingLeft: 40 }]}>
-                                <RadioButton
-                                    value='"simnao'
-                                    onPress={() => setChecked('simnao')}
-                                    status={checked === 'simnao' ? 'checked' : 'unchecked'}>
-                                </RadioButton>
-                                <Text>Sim ou Não</Text>
-                            </View>
-                            <Text style={styles.textQuestion}>E a quantidade?</Text>
-                            <TextInput style={styles.input} placeholder="2"></TextInput>
-                            <View style={{ flexDirection: "row", alignItems: "center", alignSelf: "center" }}>
-                                <TouchableOpacity style={[styles.button2, { backgroundColor: 'red', marginRight: 20, }]} onPress={() => setVisible(false)}>
-                                    <Text style={styles.textButton2}>CANCELAR</Text>
-                                </TouchableOpacity>
-                                <TouchableOpacity
-                                    style={styles.button2}
-                                    onPress={() => {
-                                        setVisible(false)
-                                    }}>
-                                    <Text style={styles.textButton2}>CRIAR</Text>
-                                </TouchableOpacity>
-                            </View>
-                        </View>
-                    </Modal>
-                    <Text style={styles.textTitle}>Código da Sessão: {codigo} </Text>
+                        </Modal>
+                        <Text style={styles.textTitle}>Código da Sessão: {codigo} </Text>
+                    </ScrollView>
                     <TouchableOpacity style={styles.button} onPress={() => setVisible(true)}>
                         <Text style={styles.textButton}>+</Text>
                     </TouchableOpacity>
                 </View>
-
             )
         } else {
             return (
-                <ScrollView>
-                    <View style={{
-                        flex: 1, justifyContent: 'flex-start', alignItems: 'center',
-                        paddingTop: Platform.OS === 'android' ? 25 : 0
-                    }}>
+                <View style={{
+                    flex: 1, justifyContent: 'flex-start', alignItems: 'center',
+                    paddingTop: Platform.OS === 'android' ? 25 : 0
+                }}>
+                    <ScrollView>
                         <Text style={styles.textTitle}>{res.nome}</Text>
                         {res.perguntas.map((pergunta, index) => {
                             return (
@@ -158,15 +191,13 @@ export default function SessionQuestions({ navigation }) {
                                     margin: 12,
                                     height: Dimensions.get('screen').height * 0.1,
                                     width: Dimensions.get('screen').width * 0.8,
-
-
                                 }}>
                                     <Text>Pergunta {index + 1}</Text>
                                 </View>
                             )
                         })}
-                    </View>
-                </ScrollView>
+                    </ScrollView>
+                </View>
             )
         }
     }
@@ -177,7 +208,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'space-between',
         alignItems: 'center',
-        paddingTop: Platform.OS === 'android' ? 25 : 0
+        paddingTop: Platform.OS === 'android' ? 25 : 0,
     },
     button: {
         alignSelf: "flex-end",
